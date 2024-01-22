8ca841712c1fa9b3f2067d2473898d66dd8c5d55
Afzal Ansari <afjalansari13998@gmail.com>
2024-01-22 15:25:25 +0530

    added higher boud code usig binary search concept

---
+++ 01Fun&Array/11HigherBound/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{1, 2, 2, 2, 3, 3, 3, 4, 4, 9, 11}
+	result := LowerBound(2, arr)
+	fmt.Println("result:::", result)
+}
+
+func LowerBound(data int, arr []int) int {
+
+	low := 0
+	high := len(arr) - 1
+	ans := -1
+
+	for low <= high {
+		mid := (low + high) / 2
+
+		if data == arr[mid] {
+			ans = mid
+			low = mid + 1
+		} else if data < arr[mid] {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	return ans
+}
