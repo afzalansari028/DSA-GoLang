fc576d4292b366e78796262b94570fc0e2c5e4bb
Afzal Ansari <afjalansari13998@gmail.com>
2024-02-14 12:50:41 +0530

    renamed

--- Test/main.go
+++ Test/main.go
@@ -1,7 +1,39 @@
 package main
 
-func main() {
-	N = 5
-	arr := make([]int, N)
+import "fmt"
 
+func main() {
+
+	arr := []int{1, 3}
+	target := 3
+	res := searchInsert(arr, target)
+	fmt.Println("res:::", res)
+}
+
+func searchInsert(nums []int, target int) int {
+	var ans int
+	var i int
+
+	if len(nums) == 0 || target <= nums[0] {
+		ans = 0
+		return ans
+	}
+
+	for i = 0; i < len(nums); i++ {
+		fmt.Println(i)
+		index := i
+		if nums[i] == target {
+			return i
+		}
+		if target > nums[index] && target < nums[index+1] {
+			return i + 1
+		}
+	}
+	fmt.Println("32")
+	if i == len(nums)-1 {
+		ans = len(nums)
+		return ans
+	}
+
+	return ans
 }
