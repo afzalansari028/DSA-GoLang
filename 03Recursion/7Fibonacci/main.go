b4daa3842883e683e6449452d5f1b4a2281711ff
Afzal Ansari <afjalansari13998@gmail.com>
2024-02-15 13:04:20 +0530

    nth fibonacci using recursion

--- 03Recursion/7Fibonacci/main.go
+++ 03Recursion/7Fibonacci/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	ans := Fibonacii(3)
+	fmt.Println("ans::", ans)
+}
+
+func Fibonacii(n int) int {
+
+	if n == 0 || n == 1 {
+		return n
+	}
+
+	Fnm1 := Fibonacii(n - 1)
+	Fnm2 := Fibonacii(n - 2)
+
+	Fib := Fnm1 + Fnm2
+	return Fib
 }
