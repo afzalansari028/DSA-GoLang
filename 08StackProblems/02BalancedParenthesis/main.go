048df763e4fe56232a8de6d4fc88e384fe07f434
Afzal Ansari <afjalansari13998@gmail.com>
2024-05-05 18:57:34 +0530

    balanced parenthesis check

--- 08StackProblems/02BalancedParenthesis/main.go
+++ 08StackProblems/02BalancedParenthesis/main.go
@@ -1,3 +1,58 @@
 package main
 
+import (
+	"fmt"
 
+	"github.com/afzalansari028/go-stack/mystack"
+)
+
+func main() {
+
+	str := "}[{()[]}]{"
+
+	if IsBalanced(str) {
+		fmt.Println("Balanced")
+	} else {
+		fmt.Println("Not balanced")
+	}
+}
+
+func IsBalanced(str string) bool {
+	var stack mystack.Stack
+
+	for i := 0; i < len(str); i++ {
+
+		ch := string(str[i])
+		// check for open brackets
+		if ch == "[" || ch == "{" || ch == "(" {
+			stack.Push(ch)
+		} else {
+			// for closing brackets
+			if stack.IsEmpty() {
+				return false
+			} else {
+				if ch == "]" {
+					if stack.Peek() == "[" {
+						stack.Pop()
+					}
+				}
+				if ch == "}" {
+					if stack.Peek() == "{" {
+						stack.Pop()
+					}
+				}
+				if ch == ")" {
+					if stack.Peek() == "(" {
+						stack.Pop()
+					}
+				}
+			}
+		}
+	}
+	stack.Display()
+	if stack.IsEmpty() {
+		return true
+	} else {
+		return false
+	}
+}
