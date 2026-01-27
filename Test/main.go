b50fcfb92a7a5a9b0e491e0c66dbeab97fa1a380
afzalansari028 <afjalansari13998@gmail.com>
2026-01-27 20:21:02 +0530

    added new function

--- Test/main.go
+++ Test/main.go
@@ -1,14 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	sl := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	srcNum := 1011
+	srcBase := 2
 
-	sl = sl[2:6]
+	result := AnyBaseToDecimal(srcNum, srcBase)
+	fmt.Println("resulta::", result)
 
-	sl[0] = 20
+	Power(2, 6)
 
-	fmt.Println("val::", sl)
-	fmt.Println("sl::", sl)
+}
+
+func AnyBaseToDecimal(srcNum, srcBase int) int {
+	res := 0
+	var k float64 = 0
+	srcB := float64(srcBase)
+	for srcNum != 0 {
+		ld := srcNum % 10
+		basewithPower := math.Pow(srcB, k)
+		res = res + ld*int(basewithPower)
+		k++
+		srcNum = srcNum / 10
+	}
+	return res
+}
+
+// Power function
+func Power(x, y int) {
+
+	var res int = 1
+	for i := 0; i < y; i++ {
+		res = res * x
+	}
+	fmt.Println("res--", res)
 }
