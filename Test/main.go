717b3656d2c0e4fd031ceb5d4ee154850c2db887
afzalansari028 <afjalansari13998@gmail.com>
2026-02-14 23:09:23 +0530

    update

--- Test/main.go
+++ Test/main.go
@@ -1,14 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+// fake db
+var users []User = []User{
+	{Id: 10, Name: "Hardik"},
+	{Id: 20, Name: "Virat"},
+	{Id: 30, Name: "Sanjuj"},
+}
+
+// interface
+type UserRepository interface {
+	GetById(id int) (User, error)
+}
+
+// user type
+type User struct {
+	Id   int
+	Name string
+}
+
+type UserService struct {
+	repo UserRepository
+}
+
+// implementation
+type fakeRepository struct{}
+
+func (f *fakeRepository) GetById(id int) (User, error) {
+
+	for _, user := range users {
+		if user.Id == id {
+			return user, nil
+		}
+	}
+	return User{}, errors.New("User not found in db")
+}
+
+// constructor - initialize user service
+func NewUserService(repo UserRepository) *UserService {
+	return &UserService{
+		repo: repo,
+	}
+}
+
+// business logic function
+func (service *UserService) GetUser(id int) (User, error) {
+
+	user, err := service.repo.GetById(id)
+	if err != nil {
+		fmt.Println("Errr occured while fetching")
+		return User{}, errors.New("Errr occured while fetching")
+	}
+	return user, nil
+}
 
 func main() {
-	sl := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	sl = sl[2:6]
+	fmt.Println("users", users)
 
-	sl[0] = 20
+	fakeRepo := &fakeRepository{}
+	service := NewUserService(fakeRepo)
 
-	fmt.Println("val::", sl)
-	fmt.Println("sl::", sl)
+	user, err := service.GetUser(10)
+	if err != nil {
+		fmt.Println("err::", err)
+		return
+	}
+	fmt.Println("user::::::", user)
+
 }
